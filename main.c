4e8b3fb029150b6bdfce3b814a055a0608973545
PenguinHaHa <smilezjxu@gmail.com>
2015-04-07 18:22:32 +0800

    add spd info function

--- main.c
+++ main.c
@@ -18,6 +18,8 @@
 #include <sys/mman.h>
 #include <stdlib.h>
 #include <getopt.h>
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
 
 typedef enum _METHOD {
   F,   // use function read/write
@@ -37,6 +39,84 @@ typedef struct _PARAMTERS {
   int    count;
 } PARAMTERS;
 
+typedef union _SPD_DATA_
+{
+   unsigned char RawData[256];
+   
+   struct _COMMON_
+   {
+      unsigned char Size;
+      unsigned char Revision;
+      unsigned char DimmType;
+   } Common;
+
+   struct _DDR_SPD_DATA_
+   {
+      unsigned char Size;              // Byte 0      - Number of Serial PD Bytes Written/SPD Device Size/CRC Coverage
+      unsigned char Revision;          // Byte 1      - SPD Revision
+      unsigned char DimmType;          // Byte 2      - DRAM Device Type
+      unsigned char NumRows;           // Byte 3      - Number of Row Addresses on this assembly
+      unsigned char NumColumns;        // Byte 4      - Number of Column Addresses on this assembly
+      unsigned char NumBanks;          // Byte 5      - Number of Physical Banks on the Dimm
+      unsigned char Width;             // Byte 6      - Module Width of this assembly
+      unsigned char ModuleOrg;         // Byte 7      - Module Organization
+      unsigned char BusWidth;          // Byte 8      - Module Memory Bus Width
+      unsigned char CycleTime;         // Byte 9      - SDRAM Device Cycle time at Max Supported CAS Latency
+      unsigned char tAC;               // Byte 10     - SDRAM Device Access from Clock (tAC)
+      unsigned char ConfigType;        // Byte 11     - DIMM Configuration Type (non-ECC = 00h, ECC = 02h)
+      unsigned char RefreshRate;       // Byte 12     - Refresh Rate/Type
+      unsigned char Pad13to17[5];      // Byte 13-17 
+      unsigned char CasLatency;        // Byte 18     - CAS Latency (CL)
+      unsigned char Pad19to20[2];      // Byte 19-20
+      unsigned char Attributes;        // Byte 21     - SDRAM Module Attributes
+      unsigned char Pad22to30[9];      // Byte 22-30
+      unsigned char Density;           // Byte 31     - Module Bank Density
+      unsigned char Pad32to62[31];     // Byte 32-62
+      unsigned char Checksum;          // Byte 63     - Checksum for byte 0 to 62
+      unsigned char JedecId[8];        // Byte 64-71  - JEDEC ID Code
+      unsigned char Location;          // Byte 72     - Manufacture Location
+      unsigned char PartNumber[18];    // Byte 73-90  - Part Number Code 
+      unsigned char RevisionCode[2];   // Byte 91-92
+      unsigned char ManufactureYear;   // Byte 93     - Manufacturing Date (year)
+      unsigned char ManufactureWeek;   // Byte 94     - Manufacturing Date (week: 01h-34h)
+      unsigned char SerialNumber[4];   // Byte 95-98  - Module Serial Number
+      unsigned char Pad99to255[157];   // Byte 99-255
+      
+   } DdrData;
+   
+   struct _DDR3_SPD_DATA_
+   {
+      unsigned char Size;              // Byte 0      - Number of Serial PD Bytes Written/SPD Device Size/CRC Coverage
+      unsigned char Revision;          // Byte 1      - SPD Revision
+      unsigned char DimmType;          // Byte 2      - DRAM Device Type
+      unsigned char Pad3;              // Byte 3
+      unsigned char Density;           // Byte 4      - SDRAM Density and Banks
+      unsigned char Pad5to6[2];        // Byte 5-6
+      unsigned char ModuleOrg;         // Byte 7      - Module Organization
+      unsigned char BusWidth;          // Byte 8      - Module Memory Bus Width
+      unsigned char Pad9;              // Byte 9
+      unsigned char MtbDividend;       // Byte 10     - Medium Timebase (MTB) Dividend
+      unsigned char MtbDivisor;        // Byte 11     - Medium Timebase (MTB) Divisor
+      unsigned char tCKCycleTime;      // Byte 12     - Minimum SDRAM Cycle Time (tCK min)
+      unsigned char Pad13;             // Byte 13
+      unsigned char CasLatencyLowByte; // Byte 14     - CAS Latencies Supported, Low Byte
+      unsigned char CasLatencyHiByte;  // Byte 15     - CAS Latencies Supported, Hi Byte
+      unsigned char Pad16to116[101];   // Byte 16-116 
+      unsigned char JedecIdLSB;        // Byte 117    - Module Manufacturer ID Code LSB
+      unsigned char JedecIdMSB;        // Byte 118    - Module Manufacturer ID Code MSB
+      unsigned char Location;          // Byte 119    - Manufacture Location
+      unsigned char ManufactureYear;   // Byte 120    - Manufacturing Date (year)
+      unsigned char ManufactureWeek;   // Byte 121    - Manufacturing Date (week: 01h-34h)
+      unsigned char SerialNumber[4];   // Byte 122-125- Module Serial Number
+      unsigned char CrcValueLSB;       // Byte 126    - SPD CRC Value
+      unsigned char CrcValueMSB;       // Byte 127    - SPD CRC Value MSB
+      unsigned char PartNumber[18];    // Byte 128-145- Module Part Number
+      unsigned char Pad146to256[110];     // Byte 146-255 
+   
+   } Ddr3Data;
+
+} SPD_DATA;
+
 #ifndef DIMENSION
 #define DIMENSION(x) (sizeof(x)/sizeof(x[0]))
 #endif
@@ -50,6 +130,11 @@ void mmapInterface(int fd, OPERATION opera, int phyAddr, int count);
 void print_usage(void);
 void parse_options(PARAMTERS *param, int argc, char** argv);
 void e820_info(void);
+void parseSpdData(char *filename, int addr, SPD_DATA *spdData);
+void spd_info(void);
+int spdReadData(int file, unsigned char *spdRawData);
+int spdReadByte(int file, int reg, unsigned char *byteData);
+int checkI2cSlaveAddr(int file);
 
 ////////////
 // LOCALS
@@ -150,6 +235,7 @@ void parse_options(PARAMTERS *param, int argc, char** argv)
 
       case 'l':
         e820_info();
+        spd_info();
         param->operation = NONE;
         break;
       
@@ -205,6 +291,106 @@ void parse_options(PARAMTERS *param, int argc, char** argv)
         break;
     }
   }while (option != -1);
+}
+
+void spd_info(void)
+{
+  char szFilename[20];
+  int  adapterNumber = 0;
+  int  file;
+  int  adapter;
+  int  addr;
+  const int i2cAddrOffset = 0xA0 >> 1;
+
+  SPD_DATA spdData;
+  
+  for(adapter = 0; adapter < 0x20; adapter++)
+  {
+    snprintf(szFilename, 29, "/dev/i2c-%d", adapterNumber);
+    file = open(szFilename, O_RDWR);
+    if (file < 0)
+    {
+      lasterror = errno;
+      printf("ERROR, open %s, %s: line %d, (%d) - %s\n", szFilename, __func__, __LINE__, lasterror, strerror(lasterror));
+      adapterNumber++;
+      continue;
+    }
+
+    addr = checkI2cSlaveAddr(file);
+//    printf("%s's addr %d\n", szFilename, addr);
+    if (addr != -1)
+    {
+      memset(spdData.RawData, 0, 256);
+      if (spdReadData(file, spdData.RawData) != -1)
+        parseSpdData(szFilename, addr, &spdData);
+    }
+
+    close(file);
+    adapterNumber++;
+  }
+}
+
+int checkI2cSlaveAddr(int file)
+{
+  int addr;
+  unsigned char data;
+  
+  for (addr = 0x50; addr < 0x58; addr++)
+  {
+    if (ioctl(file, I2C_SLAVE, addr) != 0)
+      return -1;
+
+    if (spdReadByte(file, 0, &data) == 0)
+      return addr;
+  }
+
+  return -1;
+}
+
+int spdReadData(int file, unsigned char *spdRawData)
+{
+  int reg;
+  union     i2c_smbus_data data;
+  struct    i2c_smbus_ioctl_data smbusArgs;
+
+  for (reg = 0; reg < 256; reg++)
+  {
+    if (spdReadByte(file, reg, &spdRawData[reg]) == -1)
+      return -1;
+  }
+
+  return 0;
+}
+
+int spdReadByte(int file, int reg, unsigned char *byteData)
+{
+  union     i2c_smbus_data data;
+  struct    i2c_smbus_ioctl_data smbusArgs;
+
+  smbusArgs.command    = reg;
+  smbusArgs.read_write = I2C_SMBUS_READ;
+  smbusArgs.data       = &data;
+  smbusArgs.size       = I2C_SMBUS_BYTE_DATA;
+  if (ioctl(file, I2C_SMBUS, &smbusArgs) < 0)
+  {
+//    lasterror = errno;
+//    printf("ERROR read reg %d, %s: line %d, (%d) - %s\n", reg, __func__, __LINE__, lasterror, strerror(lasterror));
+    return -1;
+  }
+  *byteData = data.byte;
+  return 0;
+}
+
+void parseSpdData(char *filename, int addr, SPD_DATA *spdData)
+{
+  int i;
+  printf("%s addr 0x%02x:", filename, addr);
+  for (i = 0; i < 256; i++)
+    printf("0x%02x ", spdData->RawData[i]);
+
+  printf("\n");
+
+  printf("Size 0x%02x, Revision 0x%02x, DimmType 0x%02x\n", spdData->Common.Size, spdData->Common.Revision, spdData->Common.DimmType);
 }
 
 void e820_info(void)
